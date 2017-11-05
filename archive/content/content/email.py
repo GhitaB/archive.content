84898a9481dc000cb3dc07a7206097d2ce72b9ce
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-11-05 14:19:53 +0200

    More fixes. WORKING now.

--- archive/content/content/email.py
+++ archive/content/content/email.py
@@ -1,11 +1,11 @@
 """ Definition of the Email dexterity content type
 """
 from zope.interface import implementer
-from archive.content.interfaces import IEmail
+from archive.content.interfaces.email import IEmail
 from plone.dexterity.content import Item
 
 
 @implementer(IEmail)
-class Infonote(Item):
+class Email(Item):
     """ Archive Email Item
     """
