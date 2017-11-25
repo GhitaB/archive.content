0e609f631216cbeda5d413b7b9844215baef39cb
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-11-25 19:13:57 +0200

    Archive email - Add new fields.

--- archive/content/interfaces/email.py
+++ archive/content/interfaces/email.py
@@ -1,7 +1,29 @@
+from collections import OrderedDict
 from collective import dexteritytextindexer
 from plone.app.textfield import RichText
 from plone.supermodel import model
+from zope import schema
 from zope.interface import Interface
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
+
+
+PRIORITY_TYPES = OrderedDict([
+    (u'1', u'1 - Low'),
+    (u'2', u'2 - Normal'),
+    (u'3', u'3 - Important'),
+    (u'4', u'4 - Very Important'),
+])
+
+
+PRIORITY_TYPES_VOCAB = SimpleVocabulary(
+    [
+        SimpleTerm(
+            value=x,
+            title=PRIORITY_TYPES[x])
+        for x in PRIORITY_TYPES.keys()
+    ]
+)
 
 
 class IArchiveContentInstalled(Interface):
@@ -13,8 +35,36 @@ class IEmail(model.Schema):
     """ Archive Email
     """
 
+    dexteritytextindexer.searchable('sender')
+    sender = schema.TextLine(
+        title=u"Sender",
+        required=True,
+        description=u"Email address"
+    )
+
+    dexteritytextindexer.searchable('receiver')
+    receiver = schema.TextLine(
+        title=u"Receiver",
+        required=True,
+        description=u"Email address"
+    )
+
     dexteritytextindexer.searchable('body')
     body = RichText(
         title=u'Email Body',
-        description=u'The Message'
+        description=u'The Message',
+        required=True
+    )
+
+    dexteritytextindexer.searchable('story')
+    story = RichText(
+        title=u'Story',
+        description=u'Some details about the context'
+    )
+
+    priority = schema.Choice(
+        title=u"Priority",
+        description=u'How important the message is',
+        vocabulary=PRIORITY_TYPES_VOCAB,
+        required=True
     )
