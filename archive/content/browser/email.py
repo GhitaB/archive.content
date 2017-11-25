0a1c4d6a08c9c0e4e4180f66d382ce4edb57eade
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-11-25 21:52:08 +0200

    Archive email sending time - human readable.

--- archive/content/browser/email.py
+++ archive/content/browser/email.py
@@ -2,9 +2,17 @@ from Products.Five.browser import BrowserView
 from archive.content.interfaces.email import PRIORITY_TYPES_VOCAB
 
 
+def human_readable_datetime(datetime_obj):
+    return datetime_obj.strftime("%d.%m.%Y %H:%M")
+
+
 class EmailView(BrowserView):
     """ Infonote View
     """
+
+    @property
+    def sending_time(self):
+        return human_readable_datetime(self.context.sending_time)
 
     @property
     def priority(self):
