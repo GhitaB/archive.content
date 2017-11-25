f777acd62b4cab88b1456edf29d9efc3fe32ee66
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-11-25 20:36:58 +0200

    Archive email view - use human readable priority.

--- archive/content/browser/email.py
+++ archive/content/browser/email.py
@@ -1,6 +1,11 @@
 from Products.Five.browser import BrowserView
+from archive.content.interfaces.email import PRIORITY_TYPES_VOCAB
 
 
 class EmailView(BrowserView):
     """ Infonote View
     """
+
+    @property
+    def priority(self):
+        return PRIORITY_TYPES_VOCAB.getTerm(self.context.priority).title
